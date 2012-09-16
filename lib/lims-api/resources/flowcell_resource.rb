5801805aa3b4733aacb87945b71ede9b9428ac83
Maxime Bourget <bmx007@gmail.com>
2012-09-16 18:30:38 +0100

    Flowcell specs pass.

--- lib/lims-api/resources/flowcell_resource.rb
+++ lib/lims-api/resources/flowcell_resource.rb
@@ -17,12 +17,10 @@ module Lims::Api
         end
       end
       def content_to_stream(s)
-        s.start_hash
         s.add_key "number_of_lanes"
         s.add_value object.number_of_lanes 
         s.add_key "lanes"
         lanes_to_stream(s)
-        s.end_hash
       end
 
       def lanes_to_stream(s)
