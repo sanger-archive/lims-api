1050dcdbd54bd67eb884b5b1f68d252b54855fb7
Karel <ke4@sanger.ac.uk>
2012-09-28 14:00:19 +0100

    [#35680209] Tube resource uses receptacle

--- lib/lims-api/resources/tube_resource.rb
+++ lib/lims-api/resources/tube_resource.rb
@@ -1,33 +1,17 @@
 #tube_resource.rb
 require 'lims-api/core_resource'
 require 'lims-api/struct_stream'
+require 'lims-api/resources/receptacle'
+
 module Lims::Api
   module Resources
     class TubeResource < CoreResource
-      def to_hash()
-        to_stream(StructStream.new).struct
-      end
-
-      def to_stream(s)
-        s.tap do
-          s.start_hash
-          s.add_key "tube"
-          content_to_stream(s)
-          s.end_hash
-        end
-      end
       
+      include Receptacle
+
       def content_to_stream(s)
         s.add_key "aliquots"
-        aliquots_to_stream(s)
-      end
-
-      def aliquots_to_stream(s)
-        s.start_array
-        object.each do |aliquot|
-          hash_to_stream(s, aliquot.attributes)
-        end
-        s.end_array
+        receptacle_to_stream(s, object)
       end
 
     end
