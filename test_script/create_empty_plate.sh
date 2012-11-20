318fcf52ed4ad636be45e0fb2070620f8176604f
Karel <ke4@sanger.ac.uk>
2012-11-20 14:52:41 +0000

    [#39541587] Modify asset creation
    The attributes for asset creation are wrapped in
    their model in the JSON.

--- test_script/create_empty_plate.sh
+++ test_script/create_empty_plate.sh
@@ -1,4 +1,4 @@
 curl -H 'Content-type:application/json' -H 'Accept:application/json' \
        	http://localhost:9292/plates \
-       	-d '{"number_of_rows":8, "number_of_columns":12}'
+       	-d '{"plate":{"number_of_rows":8, "number_of_columns":12}}'
 
