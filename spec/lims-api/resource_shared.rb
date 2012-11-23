bee5a9e249ddd6ac4eb1a6746c4865db1f67f945
Loic <llh1@sanger.ac.uk>
2012-11-23 11:07:18 +0000

    [#39857931] update order via the api spec + fix other specs

--- spec/lims-api/resource_shared.rb
+++ spec/lims-api/resource_shared.rb
@@ -1,13 +1,17 @@
 require 'lims-api/context'
 shared_context "mock context" do
-        let!(:server_context) {
-          #Context.new(store, lambda { |url| "/#{url}"  }).tap do |context|
-          mock(:context).tap do |context|
-          context.stub(:url_for)  { |url| "/#{url}"  }
-          context.stub(:recursively_lookup_uuid) { |a| a }
-          #context.stub(:last_session) { mock(:last_session) }
-          end
-        }
+  let!(:server_context) {
+    #Context.new(store, lambda { |url| "/#{url}"  }).tap do |context|
+    mock(:context).tap do |context|
+      context.stub(:url_for)  { |url| "/#{url}"  }
+      context.stub(:recursively_lookup_uuid) { |a| a }
+      #context.stub(:last_session) { mock(:last_session) }
+      context.stub(:find_model_class) { |a| Lims::Core::Organization::Order }
+      context.stub(:create_action)   
+      context.stub(:execute_action) { {} }
+      context.stub(:resource_for) { resource }
+    end
+  }
 end
 
 shared_context "with filled aliquots" do
