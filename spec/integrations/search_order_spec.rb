a8f15d0fdedf545fce4cefabbbcbf7c24be7acc4
Loic <llh1@sanger.ac.uk>
2012-12-10 09:17:06 +0000

    [#40919539] Update search order spec, fails as the search returns a wrong order uuid

--- spec/integrations/search_order_spec.rb
+++ spec/integrations/search_order_spec.rb
@@ -4,8 +4,7 @@ require 'integrations/lab_resource_shared'
 module Lims::Core
 
   shared_examples_for "search orders" do |count|
-    let(:parameters) { {:search => {:description => description, :model => searched_model, :criteria => criteria}} }
-    let(:result) { post(url, parameters.to_json) }
+    include_context "execute search"
 
     context "as a resource" do
       let(:expected_json) do
@@ -20,18 +19,29 @@ module Lims::Core
     end
 
     context "found" do
-      let(:first_result_page) { JSON.parse(result.body)["search"]["actions"]["first"] }
-      let(:result_page) { get first_result_page } 
-
       it "should be successful" do
         result.status.should == 200
       end
 
       it "should have the right number of result" do 
-        searched_order = JSON.parse(result_page.body)
-        searched_order["size"].should == count
+        orders_found["size"].should == count
       end 
     end
+  end
+
+  # works for search involving only one order back
+  shared_examples_for "retrieve the right order" do 
+    include_context "execute search"
+    let(:order_found) { orders_found["orders"].first["order"] }
+    it { order_found["uuid"].should == expected_uuid }
+  end
+
+  shared_context "execute search" do
+    let(:parameters) { {:search => {:description => description, :model => searched_model, :criteria => criteria}} }
+    let(:result) { post(url, parameters.to_json) }
+    let(:first_result_page) { JSON.parse(result.body)["search"]["actions"]["first"] }
+    let(:result_page) { get first_result_page } 
+    let(:orders_found) { JSON.parse(result_page.body) }
   end
 
   shared_context "use saved orders" do
@@ -80,6 +90,13 @@ module Lims::Core
     let(:model) { "searches" }
     let(:searched_model) { "order" }
     let(:description) { "description" }
+
+    context "search retrieves the right order" do
+      let(:criteria) { {:item => {:uuid => "1111-3333-00000000-000000000000"}} } 
+      let(:expected_uuid) { "9999-1111-00000000-000000000000" }
+      it_behaves_like "search orders", 1
+      it_behaves_like "retrieve the right order" 
+    end
 
     context "searchable by item criteria" do
       context "found 1 order" do
