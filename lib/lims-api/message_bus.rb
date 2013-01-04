247524e6aa26572c4e500b0f7356c3e5a38db414
Loic <llh1@sanger.ac.uk>
2013-01-04 14:01:39 +0000

    [#41535599] Add comments

--- lib/lims-api/message_bus.rb
+++ lib/lims-api/message_bus.rb
@@ -6,6 +6,9 @@ module Lims
     # Helper functions
     module MessageBus
       class << self
+        # Initialize the message bus, start it and 
+        # create a channel and the exchange which will be used.
+        # @param [String] environment
         def create_message_bus(env)
           config = YAML.load_file(File.join('config','amqp.yml'))[env.to_s] 
           message_bus = Lims::Core::Persistence::MessageBus.new(config)
@@ -14,6 +17,15 @@ module Lims
           message_bus.topic(config["exchange"], :durable => config["durable"])
         end
 
+        # Helper function to generate a routing key in the
+        # right format. Each part of the routing key is 
+        # separated with a point and each part contains only 
+        # letters or numbers.
+        # A routing key is build according to the following pattern:
+        # study_uuid.user_uuid.model.uuid.action
+        # @example
+        # 555223677777.66662244900.order.1111154444555.create
+        # @param [Hash] arguments
         def generate_routing_key(args)
           routing_key = ""
           %w{study_uuid user_uuid model uuid action}.each do |key|
