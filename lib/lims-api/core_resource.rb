4f3b1b5b566ea434542b600cd053121ba6569f13
Karel <ke4@sanger.ac.uk>
2013-01-23 13:17:41 +0000

    [#41182989] Fixed last_session related problems

--- lib/lims-api/core_resource.rb
+++ lib/lims-api/core_resource.rb
@@ -43,17 +43,19 @@ module Lims::Api
     end
 
     def labellable_to_stream(s, mime_type)
-      @context.last_session.tap do |session|
-        labellable = session.labellable[{:name => uuid, :type => "resource"}]
-        if labellable
-          s.add_key "labels"
-          s.with_hash do
-            resource = @context.resource_for(labellable, @context.find_model_name(labellable.class))
-
-            resource.encoder_for([mime_type]).actions_to_stream(s)
-            s.add_key "uuid"
-            s.add_value resource.uuid
-            resource.labels_to_stream(s, mime_type)
+      if @context.last_session
+        @context.last_session.tap do |session|
+          labellable = session.labellable[{:name => uuid, :type => "resource"}]
+          if labellable
+            s.add_key "labels"
+            s.with_hash do
+              resource = @context.resource_for(labellable, @context.find_model_name(labellable.class))
+  
+              resource.encoder_for([mime_type]).actions_to_stream(s)
+              s.add_key "uuid"
+              s.add_value resource.uuid
+              resource.labels_to_stream(s, mime_type)
+            end
           end
         end
       end
