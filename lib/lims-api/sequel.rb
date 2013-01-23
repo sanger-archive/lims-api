71d5db63541a06302ad556ac64284036cf50d671
max <bmx007@gmail.com>
2013-01-23 16:41:11 +0000

    Add Sql log (but comment it)

--- lib/lims-api/sequel.rb
+++ lib/lims-api/sequel.rb
@@ -2,25 +2,24 @@ require 'lims-api'
 require 'lims-core'
 require 'lims-core/persistence/sequel'
 require 'logger'
+require 'logger'
+
 module Lims
   module Api
     # Helper functions 
     module Sequel
       class << self
         def connect_db(env)
+          loggers = []
+          #loggers << Loggers.new($stdout)
           config = YAML.load_file(File.join('config','database.yml'))
-          ::Sequel.connect(config[env.to_s])
+          ::Sequel.connect(config[env.to_s], :loggers => loggers)
         end
 
         def create_store(env)
-          db = connect_db(:development)
-          db.sql_log_level = :debug
-          db.logger = Logger.new($stdout)
           store = Core::Persistence::Sequel::Store.new(connect_db(:development))
         end
       end
     end
   end
-
-
 end
