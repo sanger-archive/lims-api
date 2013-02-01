6a6a65df96143d03c4e4640575ab958ebe574a36
Loic <llh1@sanger.ac.uk>
2013-02-01 09:27:03 +0000

    [#43315783] Refactor search specs. Doesn't pass.

--- spec/integrations/search_plate_spec.rb
+++ spec/integrations/search_plate_spec.rb
@@ -3,7 +3,7 @@ require 'integrations/search_shared'
 require 'integrations/lab_resource_shared'
 
 describe "Lims::Core::Persistence::Search"do
-  include_context "use core context service", :wells, :aliquots, :plates, :samples, :searches, :labels, :labellables, :items, :orders
+  include_context "use core context service", :wells, :aliquots, :plates, :samples, :searches, :labels, :labellables, :items, :orders, :studies, :users
   include_context "JSON"
   let(:model) { "searches" }
 
@@ -21,7 +21,13 @@ describe "Lims::Core::Persistence::Search"do
 
     context "with 0 plate" do
       let(:criteria) { {:id => [1,2,5]} }
-      #    it_behaves_like "empty search"
+      it_behaves_like "empty search"
+    end
+
+    context "with some barcoded plates", :focus => true do
+      include_context "with saved assets"
+      include_context "with saved labels"
+      include_context "search by label"
     end
 
     context "with some plates" do
@@ -32,14 +38,10 @@ describe "Lims::Core::Persistence::Search"do
                                  "11111111-1111-0000-0000-000000000004"]
     end
 
-    context "with some barcoded plates" do
-      #     include_context "with saved assets"
-      #    include_context "search by label"
-    end
-
-    context "with plates in order", :focus => true do
-      #      include_context "with saved assets"
-      #      include_context "search by order" 
+    context "with plates in order" do
+      include_context "with saved assets"
+      include_context "with saved orders"
+      include_context "search by order" 
     end
   end
 end
