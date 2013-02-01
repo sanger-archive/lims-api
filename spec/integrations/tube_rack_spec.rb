d088001a749c5a1f28fab21ed2e190dc44aac476
Loic <llh1@sanger.ac.uk>
2013-02-01 12:02:30 +0000

    [#42646847] Add spec for tube rack move. Specs pass.

--- spec/integrations/tube_rack_spec.rb
+++ spec/integrations/tube_rack_spec.rb
@@ -86,6 +86,81 @@ module Lims::Core::Laboratory
     }
   end
 
+  shared_context "with a target rack tube" do |with_tube|
+    let(:target_tube_uuid) {
+      "11111111-1111-1111-4444-666666666666".tap do |uuid|
+        store.with_session do |session|
+          tube = Lims::Core::Laboratory::Tube.new
+          set_uuid(session, tube, uuid)
+        end
+      end
+    }
+
+    let!(:target_uuid) {
+      "11111111-2222-3333-4444-666666666666".tap do |uuid|
+        store.with_session do |session|
+          rack = Lims::Core::Laboratory::TubeRack.new(:number_of_rows => number_of_rows, :number_of_columns => number_of_columns)
+          rack["E3"] = session[target_tube_uuid] if with_tube
+          set_uuid(session, rack, uuid)
+        end
+      end
+    }
+  end
+
+  shared_context "expected transfer actions JSON" do
+    let(:expected_json) {
+      source_url = "http://example.org/#{uuid}"
+      target_url = "http://example.org/#{target_uuid}"
+      {
+        action_name => {
+          :actions => {},
+          :user => "user",
+          :application => "application",
+          :result => { 
+            :tube_rack => { 
+              :actions => {
+                :read => target_url,
+                :update => target_url,
+                :delete => target_url,
+                :create => target_url
+              },
+              :uuid => target_uuid,
+              :number_of_rows => number_of_rows,
+              :number_of_columns => number_of_columns,
+              :tubes => target_tubes}
+          },
+          :source => {
+            :tube_rack => {
+              :actions => {
+                :read => source_url,
+                :update => source_url,
+                :delete => source_url,
+                :create => source_url
+              },
+              :uuid => uuid,
+              :number_of_rows => number_of_rows,
+              :number_of_columns => number_of_columns,
+              :tubes => source_tubes}
+          },
+          :target => { 
+            :tube_rack => { 
+              :actions => {
+                :read => target_url,
+                :update => target_url,
+                :delete => target_url,
+                :create => target_url
+              },
+              :uuid => target_uuid,
+              :number_of_rows => number_of_rows,
+              :number_of_columns => number_of_columns,
+              :tubes => target_tubes}
+          },
+          action_map_name => transfer_map
+        }
+      }
+    }
+  end
+
 
   describe TubeRack do
     include_context "use core context service", :tube_rack_slots, :tube_aliquots, :aliquots, :samples, :tubes, :tube_racks
@@ -182,34 +257,18 @@ module Lims::Core::Laboratory
 
       context "with correct parameters" do
         include_context "with saved tube rack with tubes"
-
-        let(:target_tube_uuid) {
-          "11111111-1111-1111-4444-666666666666".tap do |uuid|
-            store.with_session do |session|
-              tube = Lims::Core::Laboratory::Tube.new
-              set_uuid(session, tube, uuid)
-            end
-          end
-        }
-
-        let!(:target_uuid) {
-          "11111111-2222-3333-4444-666666666666".tap do |uuid|
-            store.with_session do |session|
-              rack = Lims::Core::Laboratory::TubeRack.new(:number_of_rows => number_of_rows, :number_of_columns => number_of_columns)
-              rack["E3"] = session[target_tube_uuid]
-              set_uuid(session, rack, uuid)
-            end
-          end
-        }
+        include_context "with a target rack tube", true
+        include_context "expected transfer actions JSON"
 
         let(:transfer_map) { {"B5" => "E3"} }
+        let(:action_name) { "tube_rack_transfer" }
+        let(:action_map_name) { "transfer_map" }
         let(:parameters) { {
           :tube_rack_transfer => {
             :source_uuid => uuid,
             :target_uuid => target_uuid,
             :transfer_map => transfer_map}
         }}
-
         let(:source_tubes) {
           path = "http://example.org/#{tube_uuid}"
           {
@@ -221,7 +280,6 @@ module Lims::Core::Laboratory
                       "uuid" => tube_uuid,
                       "aliquots" => aliquot_array}} 
         }
-
         let(:target_tubes) {
           path = "http://example.org/#{target_tube_uuid}"
           {
@@ -233,59 +291,52 @@ module Lims::Core::Laboratory
                       "uuid" => target_tube_uuid,
                       "aliquots" => aliquot_array}}
         }
+        it_behaves_like "a valid core action"
+      end
+    end
 
-        let(:expected_json) {
-          source_url = "http://example.org/#{uuid}"
-          target_url = "http://example.org/#{target_uuid}"
+    context "#move" do
+      include_context "with filled aliquots"
+      let(:url) { "/actions/tube_rack_move" }
+
+      context "with empty parameters" do
+        let(:parameters) { {} }
+        let(:expected_json) { {
+          :errors => {
+            :source => "invalid",
+            :target => "invalid",
+            :move_map => "invalid"}
+        }}
+        it_behaves_like "an invalid core action", 422
+      end
+
+      context "with correct parameters" do
+        include_context "with saved tube rack with tubes"
+        include_context "with a target rack tube"
+        include_context "expected transfer actions JSON"
+
+        let(:transfer_map) { {"B5" => "E9"} }
+        let(:action_name) { "tube_rack_move" }
+        let(:action_map_name) { "move_map" }
+        let(:parameters) { {
+          :tube_rack_move => {
+            :source_uuid => uuid,
+            :target_uuid => target_uuid,
+            :move_map => transfer_map}
+        }}
+        let(:source_tubes) { {} }
+        let(:target_tubes) {
+          path = "http://example.org/#{tube_uuid}"
           {
-            :tube_rack_transfer => {
-              :actions => {},
-              :user => "user",
-              :application => "application",
-              :result => { 
-                :tube_rack => { 
-                  :actions => {
-                    :read => target_url,
-                    :update => target_url,
-                    :delete => target_url,
-                    :create => target_url
-                  },
-                  :uuid => target_uuid,
-                  :number_of_rows => number_of_rows,
-                  :number_of_columns => number_of_columns,
-                  :tubes => target_tubes}
-              },
-              :source => {
-                :tube_rack => {
-                  :actions => {
-                    :read => source_url,
-                    :update => source_url,
-                    :delete => source_url,
-                    :create => source_url
-                  },
-                  :uuid => uuid,
-                  :number_of_rows => number_of_rows,
-                  :number_of_columns => number_of_columns,
-                  :tubes => source_tubes}
-              },
-              :target => { 
-                :tube_rack => { 
-                  :actions => {
-                    :read => target_url,
-                    :update => target_url,
-                    :delete => target_url,
-                    :create => target_url
-                  },
-                  :uuid => target_uuid,
-                  :number_of_rows => number_of_rows,
-                  :number_of_columns => number_of_columns,
-                  :tubes => target_tubes}
-              },
-              :transfer_map => transfer_map
-            }
-          }
+            "E9" => {"actions" => 
+                     {"read" => path,
+                      "create" => path,
+                      "update" => path,
+                      "delete" => path},
+                      "uuid" => tube_uuid,
+                      "aliquots" => aliquot_array}}
         }
-
+ 
         it_behaves_like "a valid core action"
       end
     end
