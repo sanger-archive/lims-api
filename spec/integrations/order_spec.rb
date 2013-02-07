184ac9e3b68104be5670cccc496bad61a0d2afd6
Loic <llh1@sanger.ac.uk>
2013-02-07 13:42:32 +0000

    [#44003119] Refactor update item by index

--- spec/integrations/order_spec.rb
+++ spec/integrations/order_spec.rb
@@ -235,12 +235,12 @@ module Lims::Core
 
         context "with items referenced by index" do
           let(:sources) { {:source_role => [{:uuid => "44444444-0000-1111-2222-777777777777"}]} }
-          let(:targets) { {:target_role => [{:uuid => "44444444-2222-1111-2222-777777777777"}, {:uuid => "44444444-2222-2222-2222-777777777777"}]} }
+          let(:targets) { {:target_role => [{:uuid => "44444444-2222-1111-2222-777777777777"}]} }
           let(:order_items) { 
             {:source_role => [{:status => "done", :uuid => "44444444-0000-1111-2222-777777777777"}],
-             :target_role => [{:status => "pending", :uuid => "44444444-2222-1111-2222-777777777777"}, {:status => "in_progress", :uuid => "44444444-2222-2222-2222-777777777777"}]}
+             :target_role => [{:status => "in_progress", :uuid => "44444444-2222-1111-2222-777777777777"}, {:status => "pending", :uuid => nil}]}
           }
-          let(:items_update) { {:items => {:target_role => {1 => {:event => :start}}}} }
+          let(:items_update) { {:items => {:target_role => {"0" => {:event => :start}, "last" => {}}}} }
           it_behaves_like "modify order", :build, "pending"
         end
       end
