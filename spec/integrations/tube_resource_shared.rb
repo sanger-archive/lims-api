cc274c05122872422ecd6d4f6bfcbdfb5cf03194
Loic <llh1@sanger.ac.uk>
2013-02-13 10:07:17 +0000

    [#44307145] Add specs for tube type and max_volume

--- spec/integrations/tube_resource_shared.rb
+++ spec/integrations/tube_resource_shared.rb
@@ -10,8 +10,20 @@ shared_context "for tube-like asset with samples" do
   let(:aliquot_type) { "sample" }
   let(:aliquot_quantity) { 10 }
   let(:unit_type) { "mole" }
-  let(:aliquots) {{:aliquots => [ { "sample_uuid" => sample_uuid, :type => aliquot_type, :quantity => aliquot_quantity } ] }}
-  let(:parameters) { { asset => aliquots} }
+  let(:aliquots) { [ { "sample_uuid" => sample_uuid, :type => aliquot_type, :quantity => aliquot_quantity } ] }
+  let(:parameters) { { asset => {:aliquots => aliquots}} }
+end
+
+shared_context "for tube with samples" do
+  include_context "for tube-like asset with samples"
+  let(:tube_type) { "Eppendorf" }
+  let(:tube_max_volume) { 2 }
+  let(:parameters) { {asset => {:aliquots => aliquots, :type => tube_type, :max_volume => tube_max_volume}} }
+end
+
+shared_context "for tube with samples and labels" do
+  include_context "for tube with samples"
+  let(:label_parameters) {{ :labellables => labellable }}
 end
 
 shared_context "for tube-like asset with samples and labels" do
