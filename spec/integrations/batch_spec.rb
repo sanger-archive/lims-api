60f9aec21d134d4a181b3ce917bb5e5f854d6758
Loic <llh1@sanger.ac.uk>
2013-02-15 13:24:20 +0000

    [#44387863] Add specs for batch creation

--- spec/integrations/batch_spec.rb
+++ spec/integrations/batch_spec.rb
@@ -3,25 +3,6 @@ require 'integrations/lab_resource_shared'
 require 'integrations/spec_helper'
 
 module Lims::Core
-
-  shared_context "batch json" do
-    let(:expected_json) {
-      url = "http://example.org/#{uuid}"
-      {
-        "batch" => {
-          "actions" => {
-            "read" => url,
-            "create" => url,
-            "update" => url,
-            "delete" => url
-          },
-          "uuid" => uuid,
-          "process" => process 
-        }
-      }
-    }
-  end
-
   describe Organization::Order do
     include_context "use core context service", :batches
     include_context "JSON"
@@ -29,9 +10,23 @@ module Lims::Core
     context "#create" do
       context "with correct parameters" do
         include_context "use generated uuid"
-        include_context "batch json"
         let(:url) { "/batches" }
         let(:process) { "manual extraction" }
+        let(:expected_json) {
+          url = "http://example.org/#{uuid}"
+          {
+            "batch" => {
+              "actions" => {
+                "read" => url,
+                "create" => url,
+                "update" => url,
+                "delete" => url
+              },
+              "uuid" => uuid,
+              "process" => process 
+            }
+          }
+        }
         let(:parameters) { {:batch => {:process => process}} }
         it_behaves_like "a valid core action"
       end
