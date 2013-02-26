41761f9772db1713cf6888445440fe1e0617e295
Karel <ke4@sanger.ac.uk>
2013-02-26 09:37:35 +0000

    [#44711289] Add from multiply plates to 1 plate spec
    Here plate means plate-like asset as plate, gel,
    tuberack.

--- spec/integrations/transfer_plates_to_plates_spec.rb
+++ spec/integrations/transfer_plates_to_plates_spec.rb
@@ -367,7 +367,7 @@ describe Lims::Core::Actions::TransferPlatesToPlates do
         it_behaves_like "a valid core action"
       end
 
-      context "to existing target racks", :focus => true do
+      context "to existing target racks" do
         let(:target_rack1_uuid) { new_empty_tube_rack('11111111-2222-3333-1111-000000000002') }
         let(:target_rack2_uuid) { new_empty_tube_rack('11111111-2222-3333-1111-000000000003') }
 
@@ -460,6 +460,102 @@ describe Lims::Core::Actions::TransferPlatesToPlates do
 
         it_behaves_like "a valid core action"
       end
+    end
+
+    context "from a tube-rack and a plate with samples", :focus => true do
+      let(:source_rack1_uuid) { create_tube_rack_with_aliquots_and_solvent(
+        '11111111-2222-3333-1111-000000000000',
+        samples[0],
+        source_locations[0]
+        )
+      }
+      let(:source_plate1_uuid) { create_plate_with_aliquots_and_solvent(
+        '11111111-2222-3333-1111-000000000001',
+        samples[1],
+        source_locations[1]
+        )
+      }
+      context "to existing target plate" do
+        let(:target_plate1_uuid) { new_empty_plate('11111111-2222-3333-1111-000000000002') }
+
+        let(:transfers) { [
+          { "source_uuid" => source_rack1_uuid,
+            "source_location" => source_locations[0],
+            "target_uuid" => target_plate1_uuid,
+            "target_location" => target_locations[0],
+            "fraction" => 0.4,
+            "aliquot_type" => aliquot_type_DNA
+          },
+          { "source_uuid" => source_plate1_uuid,
+            "source_location" => source_locations[1],
+            "target_uuid" => target_plate1_uuid,
+            "target_location" => target_locations[1],
+            "fraction" => 0.4,
+            "aliquot_type" => aliquot_type_RNA
+          }
+          ]
+        }
+        let(:parameters) { { :transfer_plates_to_plates => { :transfers => transfers } } }
+
+        let(:expected_json) {
+          source_rack1_url = "http://example.org/#{source_rack1_uuid}"
+          source_plate1_url = "http://example.org/#{source_plate1_uuid}"
+          target_plate1_url = "http://example.org/#{target_plate1_uuid}"
+          { :transfer_plates_to_plates =>
+            { :actions => {},
+              :user => "user",
+              :application => "application",
+              :result => {
+                :sources => [
+                  { "tube_rack" => {
+                    "actions" => {
+                      "read" => source_rack1_url,
+                      "create" => source_rack1_url,
+                      "update" => source_rack1_url,
+                      "delete" => source_rack1_url
+                    },
+                    "uuid" => source_rack1_uuid,
+                    "number_of_rows" => number_of_rows,
+                    "number_of_columns" => number_of_columns,
+                    "tubes"=> tube_rack_source_elements1 }
+                  },
+                  { "plate" => {
+                    "actions" => {
+                      "read" => source_plate1_url,
+                      "create" => source_plate1_url,
+                      "update" => source_plate1_url,
+                      "delete" => source_plate1_url
+                    },
+                    "uuid" => source_plate1_uuid,
+                    "type"=>nil,
+                    "number_of_rows" => number_of_rows,
+                    "number_of_columns" => number_of_columns,
+                    "wells"=> source_elements2 }
+                  }
+                ],
+                :targets => [
+                  { "plate" => {
+                    "actions" => {
+                      "read" => target_plate1_url,
+                      "create" => target_plate1_url,
+                      "update" => target_plate1_url,
+                      "delete" => target_plate1_url
+                    },
+                    "uuid" => target_plate1_uuid,
+                    "type"=>nil,
+                    "number_of_rows" => number_of_rows,
+                    "number_of_columns" => number_of_columns,
+                    "wells"=> target_elements_B2_D4 }
+                  }
+                ]
+              },
+              :transfers => transfers
+            }
+          }
+        }
+
+        it_behaves_like "a valid core action"
+        end
     end
   end
 end
