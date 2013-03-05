012c5a64e2e4f9404e77a31f77aed7dc68510a1c
max <bmx007@gmail.com>
2013-03-05 15:26:55 +0000

    Refactor uuid sequence generation

--- spec/integrations/requests/spec_helper.rb
+++ spec/integrations/requests/spec_helper.rb
@@ -2,7 +2,7 @@ require 'integrations/spec_helper'
 
 def set_uuid_start(*ids)
   $uuid_sequence = ids.inject(0) do |m,id|
-    m = m*10 +  (id-1) 
+    m = m*10 + id 
   end
 end
 
@@ -10,17 +10,17 @@ Rspec.configure do |config|
   # Stub uuid to generete a Sequence of uuid 
   # depending on the class
   config.before(:each) do 
-    set_uuid_start(1,2,3,4,0)
     Lims::Core::Uuids::UuidResource.stub(:generate_uuid) do
       sequence = $uuid_sequence
       $uuid_sequence +=1
       ids = []
-      5.times do 
-        ids.unshift (sequence % 10)+1
+      Lims::Core::Uuids::UuidResource::Form.reverse.map do |length|
+        id = sequence % 10
         sequence /= 10
-      end
-
-      Lims::Core::Uuids::UuidResource::Form.zip(ids).map { |length, id| id.to_s*length }.join('-')
+        id.to_s*length
+      end.reverse.join('-')
     end
+
+    set_uuid_start(1,2,3,4,0)
   end
 end
