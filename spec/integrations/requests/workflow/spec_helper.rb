cea4d0342f6ef8ee79f34c38a37dcac037754a45
Loic <llh1@sanger.ac.uk>
2013-03-21 09:57:56 +0000

    [#46458581] Update specs helper

--- spec/integrations/requests/workflow/spec_helper.rb
+++ spec/integrations/requests/workflow/spec_helper.rb
@@ -3,66 +3,55 @@ require "integrations/requests/spec_helper"
 # all file in this directory and subdirectory
 
 shared_context "setup_s2_environment_for_dna_rna_manual_extraction" do
+  let(:user_uuid) { "c5657b40-743a-0130-e1ba-282066132de2" }
+  let(:study_uuid) { "c5658890-743a-0130-e1ba-282066132de2" }
+  let(:tube_uuids) { ["c566aa00-743a-0130-e1ba-282066132de2", "c5680c20-743a-0130-e1ba-282066132de2", "c5697450-743a-0130-e1ba-282066132de2"] }
+  let(:labellable_uuids) { ["c566bc20-743a-0130-e1ba-282066132de2", "c5681bd0-743a-0130-e1ba-282066132de2", "c5698510-743a-0130-e1ba-282066132de2"] }
+  let(:order_uuids) { ["c56b6ae0-743a-0130-e1ba-282066132de2", "c56cc190-743a-0130-e1ba-282066132de2"] }
+
+  let(:source_tube_barcodes) { ["1220017279667", "1220017279668", "1220017279669"] }
+
   # Needed for the order creation
   # - a valid study uuid
   # - a valid user uuid
   # Add a user and a study in the core
-  let(:order_config) { 
-    store.with_session do |session|
-      user = Lims::Core::Organization::User.new
-      session << user
-      user_uuid = session.uuid_for!(user)
-
-      study = Lims::Core::Organization::Study.new
-      session << study
-      study_uuid = session.uuid_for!(study)
-
-      lambda { {:user_id => session.id_for(user), :study_id => session.id_for(study)} }
-    end.call
+  let!(:order_config) { 
+    user = Lims::Core::Organization::User.new
+    study = Lims::Core::Organization::Study.new
+    save_with_uuid({user => user_uuid, study => study_uuid})
+    {:user_uuid => user_uuid, :study_uuid => study_uuid}
   }
 
   # Setup the orders for dna_rna manual extraction pipeline
   # 2 tubes in 1 order
   # 1 tube in 1 order
-
   # Create the tubes for dna+rna manual extraction orders
   # Barcode each tube
-  let(:source_tube_barcodes) { ["1220017279667", "1220017279668", "1220017279669"] }
-  let(:tube_manual_uuids) {
+  let!(:tube_manual_uuids) {
     [0, 1, 2].map! do |i|
-      store.with_session do |session|
-        tube = Lims::Core::Laboratory::Tube.new
-        sample = Lims::Core::Laboratory::Sample.new(:name => "sample_#{i}") 
-        tube << Lims::Core::Laboratory::Aliquot.new(:sample => sample,
-                                                    :type => "NA+P",
-                                                    :quantity => 1000)
-        session << tube
-        tube_uuid = session.uuid_for!(tube)
-
-        labellable = Lims::Core::Laboratory::Labellable.new(:name => tube_uuid, :type => "resource")
-        labellable["barcode"] = Lims::Core::Laboratory::Labellable::Label.new(:type => "ean13-barcode", 
-                                                                              :value => source_tube_barcodes[i])
-        session << labellable
-        session.uuid_for!(labellable)
-
-        lambda { tube_uuid }
-      end.call
+      tube = Lims::Core::Laboratory::Tube.new
+      sample = Lims::Core::Laboratory::Sample.new(:name => "sample_#{i}") 
+      tube << Lims::Core::Laboratory::Aliquot.new(:sample => sample,
+                                                  :type => "NA+P",
+                                                  :quantity => 1000)
+      labellable = Lims::Core::Laboratory::Labellable.new(:name => tube_uuids[i], :type => "resource")
+      labellable["barcode"] = Lims::Core::Laboratory::Labellable::Label.new(:type => "ean13-barcode", 
+                                                                            :value => source_tube_barcodes[i])
+      save_with_uuid({tube => tube_uuids[i], labellable => labellable_uuids[i]})
     end
   }
 
   # Create orders
   # First order with 2 tubes, Second order with 1 tube
-  let(:create_order) {
-    [[tube_manual_uuids[0], tube_manual_uuids[1]], [tube_manual_uuids[2]]].each do |source_tubes|
-      store.with_session do |session|
-        order = Lims::Core::Organization::Order.new(:creator => session.user[order_config[:user_id]],
-                                                    :study => session.study[order_config[:study_id]],
-                                                    :pipeline => "DNA+RNA manual extraction",
-                                                    :cost_code => "cost code")
-        order.add_source("tube_to_be_extracted_na+p", source_tubes)
-        session << order
-        session.uuid_for!(order)
-      end
+  let!(:create_order) {
+    [[tube_manual_uuids[0], tube_manual_uuids[1]], [tube_manual_uuids[2]]].each_with_index do |source_tubes, index|
+      order = Lims::Core::Organization::Order.new(:creator => order_config[:user_uuid],
+                                                  :study => order_config[:study_uuid],
+                                                  :pipeline => "DNA+RNA manual extraction",
+                                                  :cost_code => "cost code")
+      order.add_source("tube_to_be_extracted_na+p", source_tubes)
+
+      save_with_uuid({order => order_uuids[index]})
     end
   }
 end
