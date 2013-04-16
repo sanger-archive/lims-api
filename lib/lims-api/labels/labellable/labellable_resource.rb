7374aeef7c772e861f105893d99a40c07f3b4902
max <bmx007@gmail.com>
2013-04-16 11:38:19 +0100

    Fix syntax error in labellable resource

--- lib/lims-api/labels/labellable/labellable_resource.rb
+++ lib/lims-api/labels/labellable/labellable_resource.rb
@@ -7,28 +7,29 @@ require 'lims-core/labels/labellable'
 module Lims::Core
   module Labels
     class Labellable
-    class LabellableResource < Lims::Api::CoreResource
+      class LabellableResource < Lims::Api::CoreResource
 
-      def content_to_stream(s, mime_type)
-        s.add_key "name"
-        s.add_value object.name
-        s.add_key "type"
-        s.add_value object.type
-        s.add_key "labels"
-        s.with_hash do
-          labels_to_stream(s, mime_type)
-        end
-      end
-
-      def labels_to_stream(s, mime_type)
-        object.each do |position, label|
-          s.add_key position
-          s.start_hash
-          s.add_key "value"
-          s.add_value label.value
+        def content_to_stream(s, mime_type)
+          s.add_key "name"
+          s.add_value object.name
           s.add_key "type"
-          s.add_value label.type
-          s.end_hash
+          s.add_value object.type
+          s.add_key "labels"
+          s.with_hash do
+            labels_to_stream(s, mime_type)
+          end
+        end
+
+        def labels_to_stream(s, mime_type)
+          object.each do |position, label|
+            s.add_key position
+            s.start_hash
+            s.add_key "value"
+            s.add_value label.value
+            s.add_key "type"
+            s.add_value label.type
+            s.end_hash
+          end
         end
       end
     end
